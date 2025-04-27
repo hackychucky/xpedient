26a2dbb816fab61702152da383ad7d8fb19bc286
hackychucky <hackychucky@proton.me>
2025-04-27 16:30:57 +0200

    first commit, git setup

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+// src/server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
